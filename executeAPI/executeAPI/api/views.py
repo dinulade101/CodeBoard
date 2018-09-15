b86e469f99a4faa744eabc3992142974e2a097a3
Kai-Bailey <kbailey1@ualberta.ca>
2018-09-15 07:59:26 -0600

    General File Layout

--- executeAPI/executeAPI/api/views.py
+++ executeAPI/executeAPI/api/views.py
@@ -11,17 +11,26 @@ import json
 from django.http import HttpResponse
 # Create your views here.
 
-@api_view(["GET","POST"])
+@api_view(["POST"])
 def executeCode(codeData):
-    # codeData = json.loads(codeData)
 
-    # lang = codeData["language"]
-    # code = codeData["code"]
+    return codeData
+    lang_to_file = {"python":"main.py","CPP":"main.cpp", "Java":"main.java"}
+    codeData = json.loads(codeData)
+    lang = codeData["language"]
+    code = codeData["code"]
 
-    # if lang == "Python":
-    #     output = "Hello This is python"
+    if lang == "Python":
+        output = "Hello This is python"
+    elif lang == "C++":
+        output = "This is C++"
+    elif lang == "Java":
+        output = "This is Java"
+    else:
+        output = "Invalid language"
 
-    # outputData = {"lang":lang, "output":output}
 
-    # #return json.dumps(outputData)
-    return HttpResponse("<h1>This is a test")
+
+    outputData = {"lang":lang, "output":output}
+    return json.dumps(outputData)
+    #return HttpResponse("<h1>This is a test")
