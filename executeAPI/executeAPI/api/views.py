1cd970d88c9bc85c303378f4ced7a688de123f05
Kai-Bailey <kbailey1@ualberta.ca>
2018-09-15 10:03:31 -0600

    Python Dockerfile

--- executeAPI/executeAPI/api/views.py
+++ executeAPI/executeAPI/api/views.py
@@ -6,7 +6,10 @@ from rest_framework.response import Response
 from django.http import JsonResponse
 from django.core import serializers
 from django.conf import settings
+
 import json
+import os
+import subprocess
 
 from django.http import HttpResponse
 # Create your views here.
@@ -14,23 +17,26 @@ from django.http import HttpResponse
 @api_view(["POST"])
 def executeCode(codeData):
 
-    return codeData
     lang_to_file = {"python":"main.py","CPP":"main.cpp", "Java":"main.java"}
     codeData = json.loads(codeData)
     lang = codeData["language"]
     code = codeData["code"]
 
+    os.chdir("../../../codeexecution")
     if lang == "Python":
-        output = "Hello This is python"
+        codeFile = open("main.py", "w") 
     elif lang == "C++":
-        output = "This is C++"
+        codeFile = open("main.cpp", "w")
     elif lang == "Java":
-        output = "This is Java"
+        codeFile = open("main.java", "w")
     else:
         output = "Invalid language"
 
+    codeFile.write(codeFile) 
+    codeFile.close()
 
+    #subprocess.call(["docker", "run"])
 
     outputData = {"lang":lang, "output":output}
-    return json.dumps(outputData)
+    return JsonResponse(outputData)
     #return HttpResponse("<h1>This is a test")
