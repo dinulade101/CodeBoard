6fa64c5255a4fe456866f03a605095029e4493f8
fbulbuc <fbulbuc@ualberta.ca>
2018-09-15 02:24:57 -0400

    Added empty activities.

---
+++ Android/app/src/main/java/com/codeboard/htn/codeboard/editor/CodeEditorActivity.java
@@ -0,0 +1,15 @@
+package com.codeboard.htn.codeboard.editor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codeboard.htn.codeboard.R;
+
+public class CodeEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_editor);
+    }
+}
--- Android/app/src/main/java/com/codeboard/htn/codeboard/explorer/ScriptExplorerActivity.java
+++ Android/app/src/main/java/com/codeboard/htn/codeboard/explorer/ScriptExplorerActivity.java
@@ -1,4 +1,4 @@
-package com.codeboard.htn.codeboard;
+package com.codeboard.htn.codeboard.explorer;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.codeboard.htn.codeboard.R;
 
 public class ScriptExplorerActivity extends AppCompatActivity {
 
